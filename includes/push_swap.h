eb705565f63b86feb775c2cc6589b1c6674fe418
markoscomks <markoscomks@gmail.com>
2024-08-20 14:05:02 +0100

    Basic header file

---
+++ includes/push_swap.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/20 14:03:40 by marsoare          #+#    #+#             */
+/*   Updated: 2024/08/20 14:04:17 by marsoare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdbool.h>
+# include "../libft/libft.h"
+
+#endif
