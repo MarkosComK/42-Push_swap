3ada1ec582cafc0c0462c2287bb4961f10b9f712
markoscomks <markoscomks@gmail.com>
2024-08-21 16:19:54 +0100

    Added function to handle case av[1] = 'example args'

--- srcs/push_swap/push_swap.c
+++ srcs/push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 14:01:51 by marsoare          #+#    #+#             */
-/*   Updated: 2024/08/21 15:50:29 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/08/21 16:18:22 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,19 +24,26 @@ void	print_stack(t_stack *stack)
 	}
 }
 
+int	split_len(char **tmp)
+{
+	int	i;
+
+	i = 0;
+	while (tmp[i])
+		i++;
+	return (i);
+}
+
 int	main(int ac, char **av)
 {
 	t_stack	*a;
 
 	if (ac < 2)
 		ft_error();
-	/*
 	if (ac == 2)
-		function_to_handle();
-	*/
-	a = stack_new(ac, av);
-	if (ac < 2)
-		ft_error();
+		a = stack_new(split_len(ft_split(av[1], ' ')), ft_split(av[1], ' '));
+	else
+		a = stack_new(ac, av);
 	print_stack(a);
 	stack_free(a);
 	return (0);
