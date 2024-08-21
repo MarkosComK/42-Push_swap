1fd641162ed3511879fb97e6beec31fa427023c1
markoscomks <markoscomks@gmail.com>
2024-08-21 18:22:02 +0100

    Added function to check for duplicates in stack A

--- srcs/push_swap/push_swap.c
+++ srcs/push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 14:01:51 by marsoare          #+#    #+#             */
-/*   Updated: 2024/08/21 18:07:05 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/08/21 18:20:19 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,12 +31,22 @@ int	main(int ac, char **av)
 
 	if (ac < 2)
 		ft_error();
-	excep_ac = split_len(ft_split(av[1], ' '));
 	if (ac == 2)
+	{
+		excep_ac = split_len(ft_split(av[1], ' '));
 		a = stack_exception(excep_ac, ft_split(av[1], ' '));
+	}
 	else
 		a = stack_new(ac, av);
-	print_stack(a);
+	if (!a || stack_check_dup(a))
+	{
+		stack_free(a);
+		ft_error();
+	}
+	/*
+	if (!ft_checksorted(a))
+		ft_sort(&a);
+	*/
 	stack_free(a);
 	return (0);
 }
--- srcs/push_swap/stack_utils.c
+++ srcs/push_swap/stack_utils.c
@@ -6,11 +6,29 @@
 /*   By: marsoare <marsoare@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 15:37:33 by marsoare          #+#    #+#             */
-/*   Updated: 2024/08/21 15:39:29 by marsoare         ###   ########.fr       */
+/*   Updated: 2024/08/21 18:16:22 by marsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
+
+int	stack_check_dup(t_stack *stack)
+{
+	t_stack	*tmp;
+
+	while (stack)
+	{
+		tmp = stack->next;
+		while (tmp)
+		{
+			if (stack->nbr == tmp->nbr)
+				return (1);
+			tmp = tmp->next;
+		}
+		stack = stack->next;
+	}
+	return (0);
+}
 
 t_stack	*stack_last(t_stack *stack)
 {
